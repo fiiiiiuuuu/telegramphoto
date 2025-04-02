0078de41445f70b9398b04131b3f659a9c12f026
fiiiiiuuuu <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-04-02 16:15:38 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,17 @@
 import os
 import dotenv
 import telegram
+import random
 
 dotenv.load_dotenv('.env')
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 
 bot = telegram.Bot(token=TG_BOT_TOKEN)
 
-bot.send_message(chat_id='-1002313823580', text="Ы")
+chat_id = "-1002313823580"
+
+photo_list = os.listdir('photos')
+
+random_photo = os.path.join('photos', random.choice(photo_list))
+
+bot.send_photo(chat_id=chat_id, photo=open(random_photo, "rb"))
