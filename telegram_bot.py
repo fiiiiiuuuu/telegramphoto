00588f4022d9230ff5d0a760d4447c12c6cc4a76
fiiiiiuuuu <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-04-04 16:25:08 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,16 +2,27 @@ import os
 import dotenv
 import telegram
 import random
+import argparse
+import time
 
 dotenv.load_dotenv('.env')
 TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
+TG_CHAT_ID = os.getenv("TG_CHAT_ID")
 
 bot = telegram.Bot(token=TG_BOT_TOKEN)
 
-chat_id = "-1002313823580"
+chat_id = TG_CHAT_ID
 
 photo_list = os.listdir('photos')
 
-random_photo = os.path.join('photos', random.choice(photo_list))
+parser = argparse.ArgumentParser() 
+parser.add_argument('--delay', type=int)
+args = parser.parse_args()
 
-bot.send_photo(chat_id=chat_id, photo=open(random_photo, "rb"))
+delay = args.delay if args.delay else 14400
+
+while True:
+    random_photo = os.path.join('photos', random.choice(photo_list))
+    bot.send_photo(chat_id=chat_id, photo=open(random_photo, "rb"))
+    time.sleep(delay)
+    random.shuffle(photo_list)
