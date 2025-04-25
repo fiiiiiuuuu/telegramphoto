eb768962305e60c0bd1f97fe8815c9d333effcfc
fiiiiiuuuu <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-04-25 16:46:00 +0300

    added try except

--- telegram_bot.py
+++ telegram_bot.py
@@ -20,9 +20,27 @@ if __name__ == '__main__':
     parser.add_argument('--delay', type=int, default=14400, help='Задрежка отправки сообщений')
     args = parser.parse_args()
 
+    max_retries = 5
+    retry_delay = 5
+
     while True:
         random_photo = os.path.join(folder, random.choice(photo_list))
-        with open(random_photo, "rb") as file:
-            bot.send_photo(chat_id=chat_id, photo=file)
+        for attemp in range(max_retries):
+            try:
+                with open(random_photo, "rb") as file:
+                    bot.send_photo(chat_id=chat_id, photo=file)
+                break
+            except telegram.error.NetworkError as e:
+                print(f'Ошибка соединения (попытка {attemp + 1}/{max_retries}): {e}')
+                if attemp < max_retries -1:
+                    print(f'Повторная попытка через {retry_delay} секунд...')
+                    time.sleep(retry_delay)
+                    retry_delay *= 2
+                continue
+            except Exception or telegram.error as e:
+                print(f'Непредвиденная ошибка: {e}')
+                break
+
         time.sleep(args.delay)
         random.shuffle(photo_list)
+        retry_delay = 5
