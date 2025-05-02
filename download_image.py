46ba1a2727fb73d12ecba8bea4c87f98e219a9bd
fiiiiiuuuu <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-05-02 15:28:08 +0300

    вынес загрузку изображений в отдельный скрипт

---
+++ download_image.py
@@ -0,0 +1,8 @@
+import requests
+
+def download_image(url, path):
+    photo_response = requests.get(url)
+    photo_response.raise_for_status()
+
+    with open(path, 'wb') as file:
+        file.write(photo_response.content)
