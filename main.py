b7eac7831fdb1bd276f52bcec65c4f85e07c938b
fiiiiiuuuu <157232546+fiiiiiuuuu@users.noreply.github.com>
2025-05-14 16:41:53 +0300

    добавил main()

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ from fetch_spacex_images import fetch_spacex_launch
 from nasa_apod_images import download_nasa_apod
 from nasa_epic_images import download_nasa_epic
 
-dotenv.load_dotenv('.env')
-NASA_API_KEY = os.environ["NASA_API_KEY"]
 
-if __name__ == "__main__":
-
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fld', help='название папки') 
-    parser.add_argument('--lid', help='айди для фото запусков spacex (необязательно)')
-    parser.add_argument('--spacex', action='store_true', help='загрузка фоток SpaceX (если нету своего айди запуска)')    
+    parser.add_argument('--fld', default='photos', help='название папки (необязательно)') 
+    parser.add_argument('--lid', nargs='?', const='5eb87d47ffd86e000604b38a', default=None, help='айди для фото запусков spacex (необязательно)')    
     parser.add_argument('--apod', action='store_true', help='загрузка картинок дня')
     parser.add_argument('--epic', action='store_true', help='загрузка фотографий земли')
     args = parser.parse_args()
@@ -21,9 +17,15 @@ if __name__ == "__main__":
     folder = args.fld
     os.makedirs(folder, exist_ok=True)
 
-    if args.lid or args.spacex:
+    if args.lid:
         fetch_spacex_launch(args.lid, folder)
     if args.apod:
         download_nasa_apod(NASA_API_KEY, folder)
     if args.epic:
         download_nasa_epic(NASA_API_KEY, folder)
+
+if __name__ == "__main__":
+    dotenv.load_dotenv('.env')
+    NASA_API_KEY = os.environ["NASA_API_KEY"]
+
+    main() 
